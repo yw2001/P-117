f6087a9dac7ee3cb95c5f05e9f68c020aacb558e
yw2001 <84959633+yw2001@users.noreply.github.com>
2022-05-10 12:04:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,80 @@ random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
      answer_holder = ""; 
      score = 0;
 
+     function updateCanvas() {
+          background("white");
+           random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+            console.log(quick_draw_data_set[random_number]);
+             sketch = quick_draw_data_set[random_number];
+              document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+             }
+
+ function preload() {
+    classifier = ml5.imageClassifier('DoodleNet')
+            }
+
      
 function setup() {
     canvas = createCanvas(280, 280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+check_sketch()
+ if(drawn_sketch == sketch) 
+ {
+      answer_holder = "set" score++;
+       document.getElementById('score').innerHTML = 'Score: ' + score; 
+    }
+ }
+
+ function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    console.log(results);
+
+    drawn_sketch = results[0].label;
+    document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch;
+
+
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
 
 function clearCanvas() {
 
     background("white");
 }
+
+function check_sketch() {
+     timer_counter++; 
+     document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
+      console.log(timer_counter) if(timer_counter > 400)
+       {
+            timer_counter = 0;
+             timer_check = "completed" } 
+             if(timer_check =="completed" |
+             | answer_holder == "set") 
+             { timer_check = "";
+              answer_holder = "";
+               updateCanvas(); 
+            } 
+            }
